9d2f054604daa574aea72f96e5d52317332152f4
Juhui Heo <82089499+JuHuiHeo@users.noreply.github.com>
2023-07-05 11:11:01 +0900

    메인 카테고리 변경

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,33 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
+// 노드 생성 시 호출되는 함수
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
+  // 만약 노드의 타입이 MarkdownRemark인 경우에만 처리
   if (node.internal.type === `MarkdownRemark`) {
+    // 마크다운 파일의 경로를 생성
     const slug = createFilePath({ node, getNode, basePath: `content` });
+    // 노드에 slug라는 필드를 추가하여 경로 저장
     createNodeField({ node, name: `slug`, value: slug });
   }
 };
 
+// 블로그 페이지 생성 함수
 const createBlogPages = ({ createPage, results }) => {
+  // 블로그 포스트 템플릿 파일 경로
   const blogPostTemplate = require.resolve(`./src/templates/blog-template.js`);
-  results.data.allMarkdownRemark.edges.forEach(({ node, next, previous }) => {
+  // GraphQL 쿼리 결과에서 데이터 추출
+  const { edges } = results.data.allMarkdownRemark;
+
+  // 각 노드에 대해 페이지 생성
+  edges.forEach(({ node, next, previous }) => {
     createPage({
+      // 경로 설정
       path: node.fields.slug,
+      // 페이지에 사용할 컴포넌트 설정
       component: blogPostTemplate,
       context: {
-        // additional data can be passed via context
+        // 페이지 컴포넌트에 전달할 추가 데이터 설정
         slug: node.fields.slug,
         nextSlug: next?.fields.slug ?? '',
         prevSlug: previous?.fields.slug ?? '',
@@ -24,26 +36,35 @@ const createBlogPages = ({ createPage, results }) => {
   });
 };
 
+// 카테고리 페이지 생성 함수
 const createPostsPages = ({ createPage, results }) => {
+  // 카테고리 템플릿 파일 경로
   const categoryTemplate = require.resolve(`./src/templates/category-template.js`);
-  const categorySet = new Set(['All']);
+  // 중복을 허용하지 않는 Set 객체 생성
+  const categorySet = new Set();
+  // GraphQL 쿼리 결과에서 데이터 추출
   const { edges } = results.data.allMarkdownRemark;
 
+  // 모든 노드의 카테고리를 Set에 추가
   edges.forEach(({ node }) => {
     const postCategories = node.frontmatter.categories.split(' ');
     postCategories.forEach((category) => categorySet.add(category));
   });
 
+  // Set을 배열로 변환하여 카테고리 목록 생성
   const categories = [...categorySet];
-  const initialCategory = 'Study'; // 원하는 초기 카테고리를 설정합니다.
 
+  // 초기 카테고리를 'Study'로 설정
+  const initialCategory = 'Study';
+
+  // 전체 카테고리 페이지 생성
   createPage({
     path: `/posts`,
     component: categoryTemplate,
     context: { currentCategory: initialCategory, edges, categories },
-    //'All'
   });
 
+  // 각 카테고리별 페이지 생성
   categories.forEach((currentCategory) => {
     createPage({
       path: `/posts/${currentCategory}`,
@@ -51,52 +72,12 @@ const createPostsPages = ({ createPage, results }) => {
       context: {
         currentCategory,
         categories,
+        // 해당 카테고리에 속한 노드들만 추출하여 페이지에 전달
         edges: edges.filter(({ node }) => node.frontmatter.categories.includes(currentCategory)),
       },
     });
   });
 };
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions;
-
-  const results = await graphql(`
-    {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
-        edges {
-          node {
-            id
-            excerpt(pruneLength: 500, truncate: true)
-            fields {
-              slug
-            }
-            frontmatter {
-              categories
-              title
-              date(formatString: "MMMM DD, YYYY")
-            }
-          }
-          next {
-            fields {
-              slug
-            }
-          }
-          previous {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  // Handle errors
-  if (results.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return;
-  }
-
-  createBlogPages({ createPage, results });
-  createPostsPages({ createPage, results });
-};
+// Gatsby 노드 생성 시 호출되는 함수
+exports.onCreateNode
